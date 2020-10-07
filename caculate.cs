863308c25f3a93a5938d0d2baa1a735ebd4bc6e0
userc048 <19853096661@163.com>
2020-10-07 08:34:25 +0800

    “caculate.cs is created

---
+++ caculate.cs
@@ -0,0 +1,15 @@
+public static class caculate
+{
+        public int add(int a,int a)
+        {
+                return a+b;
+        }
+        public int max(int a,int b)
+        {
+                if(a>b)
+                        return a;
+                else
+                        return b;
+        }
+}
+
